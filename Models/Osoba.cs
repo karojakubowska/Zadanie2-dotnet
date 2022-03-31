3a75e106358f117edb7ab99f2fc4c532f7cb94af
Karolina Jakubowska <kuki2000j@icloud.com>
2022-03-31 22:06:23 +0200

    Session and forms

---
+++ Models/Osoba.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LataPrzestepne.Models
+{
+    public class Osoba
+    {
+        [Display(Name = "Rok")]
+        [Required(ErrorMessage = "Pole Rok jest obowiązkowe"), Range(1899, 2022, ErrorMessage = "Oczekiwana wartość z zakresu {1} i {2}.")]
+        public int Rok { get; set; }
+        [Display(Name = "Imię")]
+        [StringLength(100, MinimumLength = 1)]
+        [Required(ErrorMessage = "Pole Imię jest obowiązkowe")]
+        public string Imie { get; set; }
+
+
+        public string CzyRokPrzestepny()
+        {
+            if (Rok % 4 == 0 && !(Rok % 100 == 0) || Rok % 400 == 0) 
+            {
+                return Imie+" urodził się w " + Rok + " roku. To był rok przestępny";
+            }
+            return  Imie + " urodził się w " + Rok + " roku. To nie był rok przestępny";
+        }
+
+        public bool RokPrzestepny()
+        {
+            if (Rok % 4 == 0 && !(Rok % 100 == 0) || Rok % 400 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using LataPrzestepne.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+
+namespace LataPrzestepne.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        [BindProperty]
+        public Osoba Osoba { get; set; }
+        public List<Osoba> Osoby = new List<Osoba>();
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            TempData["Results"] = Osoba.CzyRokPrzestepny();
+
+            if (HttpContext.Session.GetString("DataList") == null)
+            {
+                Osoby.Add(Osoba);
+                HttpContext.Session.SetString("DataList", JsonConvert.SerializeObject(Osoby));
+            }
+            else 
+            { 
+                var SessionList = HttpContext.Session.GetString("DataList");
+                Osoby = JsonConvert.DeserializeObject<List<Osoba>>(SessionList);
+                Osoby.Add(Osoba);
+                HttpContext.Session.SetString("DataList", JsonConvert.SerializeObject(Osoby));
+            }
+            return Page(); ;
+        }
+    }
+}
